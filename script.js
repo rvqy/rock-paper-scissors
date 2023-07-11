2f848fa4d78246ea219e4aa0ffdd11de3205ce9e
rvqy <qvadis15@gmail.com>
2023-07-11 08:01:05 +0200

    Add function for playing single round

--- script.js
+++ script.js
@@ -3,3 +3,25 @@ const shapes = ["rock", "paper", "scissors"];
 function getComputerChoices() {
     return shapes[(Math.floor(Math.random() * shapes.length))];
 }
+
+function playRound(playerSelection = "rock", computerSelection = getComputerChoices()) {
+    playerSelection = playerSelection.toLowerCase();
+    let message = "";
+
+    if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+        message = `You win! ${playerSelection} beats ${computerSelection}!`;
+    } else if (playerSelection === computerSelection) {
+        message = "Tie!";
+    } else {
+        message = `You lost! ${computerSelection} beats ${playerSelection}.`;
+    }
+
+    return message;
+}
+
+
+
